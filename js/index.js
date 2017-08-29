677e69c3edbb9c196cfc3cf3423b4553cb11f273
wbh1234 <18249214675@163.com>
2017-08-29 22:58:59 +0800

    person website

---
+++ js/index.js
@@ -0,0 +1,26 @@
+/**
+ * Created by ASUS on 2017/8/29.
+ */
+(function(){
+        var ROW = 4, COL = 3, NUM = ROW * COL;
+
+
+
+        for(var i=0; i<NUM; i++){
+            var oImg = new Image();
+            oImg.src = "img/"+(i+1)+".jpg";
+        }
+
+
+        // var oImg = new Image();
+        // oImg.src ="img/1.jpg";
+        // document.body.appendChild(oImg);
+
+
+
+
+
+
+
+
+})();
